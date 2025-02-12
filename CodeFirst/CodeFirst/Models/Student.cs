503bba627bfef024c141f8cc152ff3362f282d5c
Fresil-ZimbsTech <fresil.zimbstech@gmail.com>
2025-02-12 17:35:42 +0530

    update folder code first

---
+++ CodeFirst/CodeFirst/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirst.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column("StudentName",TypeName = "varchar(100)")]
+        public string Name { get; set; }
+        [Required]
+        [Column("StudentGender", TypeName = "varchar(20)")]
+
+        public string Gender { get; set; }
+        [Required]
+        public int? Age { get; set; }
+        [Required]
+        public int? Standard { get; set; }
+    }
+}
